6309dfc37ef9ee75747d004f8c25281c5eceaa01
ottomicheletti <michelettiotto@gmail.com>
2022-02-11 14:18:08 -0300

    Branch para desenvolvimento do JavaScript

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const { request } = require("@octokit/request");
 
-const fetchMembers = async(page) => {
-  const response = await request("GET /orgs/{org}/members", {
-    headers: {
-      authorization: "token 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",
-    },
-    org: "Tryber",
-    type: "private",
-    role: "member",
-    per_page: 30,
-    page: page,
-  });
+const fetchMembers = async () => {
+  const trybeMembers = [];
+  
+  for(let index = 1; index < 120; index += 1) {
+  
+    const response = await request("GET /orgs/{org}/members", {
+      headers: {
+        authorization: "token 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",
+      },
+      org: "Tryber",
+      type: "private",
+      role: "member",
+      per_page: 30,
+      page: index,
+    });
+  
+    const {data} = response;
 
-  const { data } = response;
+    if (data.length > 0) {
+      data.forEach((member) => trybeMembers.push(member));
+    }
+  }
+  return trybeMembers;
+}
 
- return data;
-};
-
-fetchMembers(1).then(console.log);
+fetchMembers().then(console.log);
