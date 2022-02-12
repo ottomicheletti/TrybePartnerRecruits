94c2e34362815836b403aadb56d0a7909cf03adc
ottomicheletti <michelettiotto@gmail.com>
2022-02-12 16:35:20 -0300

    Ideia de como buscar as pessoas por Turma

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { request } = require("@octokit/request");
 
 
-const fetchMembers = async () => {
-  const trybeMembers = [];
-  let pageCounter = 1;
+const fetchMembers = async (page = 1) => {
+  
   const response = await request("GET /orgs/{org}/members", {
     headers: {
       authorization: "token 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",
@@ -12,17 +11,10 @@ const fetchMembers = async () => {
     type: "private",
     role: "member",
     per_page: 30,
-    page: pageCounter,
+    page: page,
   });
 
-  const {data} = response;
-
-  while (data.length > 0) {
-    data.forEach((member) => trybeMembers.push(member));
-    pageCounter += 1;
-  }
-
-  return trybeMembers;
+  return response.data;
 }
 
 
