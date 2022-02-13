0e1eae761ca9f94a404bb4b0f476de506721461c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-13 06:49:15 -0300

    Finaliza dev-table-sort.

--- script.js
+++ script.js
@@ -12,28 +12,24 @@ const insertPlayerData = (arrayData) => {
 // funcao apresentada em https://www.w3schools.com/howto/howto_js_sort_table.asp
 function sortTable(event) {
     n = event.target.getAttribute('index');
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("myTable");
-    switching = true;
+    let rows, i, x, y, shouldSwitch, switchcount = 0;
+    const table = document.getElementById("myTable");
+    let switching = true;
     // Set the sorting direction to ascending:
-    dir = "asc";
-    /* Make a loop that will continue until
-    no switching has been done: */
+    let dir = "asc";
+    /* Make a loop that will continue until no switching has been done: */
     while (switching) {
         // Start by saying: no switching is done:
         switching = false;
         rows = table.rows;
-        /* Loop through all table rows (except the
-        first, which contains table headers): */
+        /* Loop through all table rows (except the first, which contains table headers): */
         for (i = 1; i < (rows.length - 1); i++) {
             // Start by saying there should be no switching:
             shouldSwitch = false;
-            /* Get the two elements you want to compare,
-            one from current row and one from the next: */
+            /* Get the two elements you want to compare, one from current row and one from the next: */
             x = rows[i].getElementsByTagName("TD")[n];
             y = rows[i + 1].getElementsByTagName("TD")[n];
-            /* Check if the two rows should switch place,
-            based on the direction, asc or desc: */
+            /* Check if the two rows should switch place, based on the direction, asc or desc: */
             if (dir == "asc") {
                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
                     // If so, mark as a switch and break the loop:
@@ -49,15 +45,13 @@ function sortTable(event) {
             }
         }
         if (shouldSwitch) {
-            /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
+            /* If a switch has been marked, make the switch and mark that a switch has been done: */
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
             // Each time a switch is done, increase this count by 1:
             switchcount++;
         } else {
-            /* If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again. */
+            /* If no switching has been done AND the direction is "asc", set the direction to "desc" and run the while loop again. */
             if (switchcount == 0 && dir == "asc") {
                 dir = "desc";
                 switching = true;
